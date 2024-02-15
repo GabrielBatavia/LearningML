9aede6ee602798a9777ca043af59584c16a17946
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-02-15 23:45:57 +0700

    F

--- TrashCode/print('Apa').py
+++ TrashCode/print('Apa').py
@@ -115,5 +115,6 @@ print ('Commit day 15,1')
 print ('Commit day 15,1')
 print ('Commit day 15,1')
 print ('Commit day 16,1')
-print ('Commit day 15,1')
+print ('Commit day 16,1')
+print ('Commit day 16,1')
 
